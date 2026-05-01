0c6f5522825781eff0afccba020bd869599dc041
Késsia Silva <kessiasilvadealmeida@gmail.com>
2026-04-30 23:05:42 -0300

    trigger feita

---
+++ MSQL/Trigger.sql
@@ -0,0 +1,13 @@
+DELIMITER $$ 
+             CREATE TRIGGER trg_impede_delete
+             BEFORE DELETE ON Disciplina
+             FOR EACH ROW 
+             BEGIN 
+                      IF EXISTS(SELECT 1 FROM Curso_Disciplina WHERE idDisciplina = OLD.idDisciplina ) THEN
+                      SIGNAL SQLSTATE '45000'
+                      SET MESSAGE_TEXT = 'Não pode excluir, disciplina pertence a um curso';
+                    END IF;
+                    END $$ 
+DELIMITER ;
+
+
